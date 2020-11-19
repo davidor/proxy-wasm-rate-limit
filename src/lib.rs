4f3c8a95a1eef1b58fe4482233a93b9a97aae04b
David Ortiz <z.david.ortiz@gmail.com>
2020-11-19 12:08:06 +0100

    Update to limitador v0.1.3

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,8 @@ use limitador::RateLimiter;
 use proxy_wasm::hostcalls::get_current_time;
 use proxy_wasm::traits::*;
 use proxy_wasm::types::*;
-use std::collections::HashMap;
-use std::time::{Duration, SystemTime};
+use std::collections::{HashMap, HashSet};
+use std::time::SystemTime;
 
 // Assume that everything belongs to the same namespace for now
 const NAMESPACE: &str = "proxy_wasm";
@@ -36,24 +36,26 @@ impl Limiter {
     // This is not efficient, we need to find a way to keep this state across
     // requests without having to serialize/deserialize everything.
 
-    pub fn get_stored_counters(&self) -> Option<HashMap<Counter, (i64, SystemTime)>> {
+    pub fn get_stored_counters(&self) -> Option<HashMap<Counter, SystemTime>> {
         let (stored_data, _) = self.get_shared_data(SHARED_DATA_KEY);
 
         match stored_data {
-            Some(data) => Some(
-                bincode::deserialize::<HashMap<Counter, (i64, SystemTime)>>(&data[..]).unwrap(),
-            ),
+            Some(data) => {
+                Some(bincode::deserialize::<HashMap<Counter, SystemTime>>(&data[..]).unwrap())
+            }
             None => None,
         }
     }
 
-    pub fn store_counters(&self, counters: Vec<(Counter, i64, Duration)>) {
-        let mut counters_to_store: HashMap<Counter, (i64, SystemTime)> = HashMap::new();
+    pub fn store_counters(&self, counters: HashSet<Counter>) {
+        let mut counters_to_store: HashMap<Counter, SystemTime> = HashMap::new();
 
-        for (counter, val, time_remaining) in counters {
+        for counter in counters {
             counters_to_store.insert(
                 counter.clone(),
-                (val, get_current_time().unwrap() + time_remaining),
+                // We can unwrap() expires_in() because the call to
+                // get_counters() always sets it.
+                get_current_time().unwrap() + counter.expires_in().unwrap(),
             );
         }
 
@@ -69,15 +71,15 @@ impl Limiter {
 impl HttpContext for Limiter {
     fn on_http_request_headers(&mut self, _: usize) -> Action {
         let clock = Box::new(WasmClock {});
-        let mut storage = WasmStorage::new(clock);
+        let storage = WasmStorage::new(clock);
 
-        let mut limiter = match self.get_stored_counters() {
+        let limiter = match self.get_stored_counters() {
             Some(stored_counters) => {
-                stored_counters
-                    .iter()
-                    .for_each(|(counter, (value, expires_at))| {
-                        storage.add_counter(counter, *value, *expires_at);
-                    });
+                stored_counters.iter().for_each(|(counter, expires_at)| {
+                    // We know that counter.remaining() has been set because
+                    // get_counters() always set it, so we can unwrap().
+                    storage.add_counter(counter, counter.remaining().unwrap(), *expires_at);
+                });
 
                 new_limiter(storage)
             }
@@ -86,14 +88,14 @@ impl HttpContext for Limiter {
 
         let kv_for_auth = key_vals_for_authorizing(&self.get_http_request_headers());
 
-        match limiter.is_rate_limited(&kv_for_auth, 1) {
+        match limiter.is_rate_limited(NAMESPACE, &kv_for_auth, 1) {
             Ok(is_limited) => {
                 if is_limited {
                     self.send_http_response(429, vec![], Some(b"Too many requests.\n"));
                     Action::Pause
                 } else {
-                    limiter.update_counters(&kv_for_auth, 1).unwrap();
-                    self.store_counters(limiter.get_counters(NAMESPACE));
+                    limiter.update_counters(NAMESPACE, &kv_for_auth, 1).unwrap();
+                    self.store_counters(limiter.get_counters(NAMESPACE).unwrap());
                     Action::Continue
                 }
             }
@@ -110,7 +112,6 @@ impl HttpContext for Limiter {
 /// - Request path: req.path
 /// - Request method: req.method
 /// - Request headers: req.headers._name_of_the_header_
-/// - Namespace: namespace
 fn key_vals_for_authorizing(request_headers: &[(String, String)]) -> HashMap<String, String> {
     let mut result: HashMap<String, String> = HashMap::new();
 
@@ -129,8 +130,6 @@ fn key_vals_for_authorizing(request_headers: &[(String, String)]) -> HashMap<Str
         }
     }
 
-    result.insert("namespace".to_string(), NAMESPACE.to_string());
-
     result
 }
 
@@ -141,10 +140,10 @@ fn new_limiter(storage: WasmStorage) -> RateLimiter {
     let limits = include!("limits.rs");
 
     let b: Box<dyn Storage> = Box::new(storage);
-    let mut limiter = RateLimiter::new_with_storage(b);
+    let limiter = RateLimiter::new_with_storage(b);
 
     for limit in limits {
-        limiter.add_limit(limit.clone()).unwrap();
+        limiter.add_limit(&limit).unwrap();
     }
 
     limiter
